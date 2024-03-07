c8237460c22aad9b0b63b45c414289bbc6a000e0
Jason Lethbridge <lethbridgejason@gmail.com>
2024-03-07 23:35:21 +1100

    Give error descriptions in the body for CACHE and GET requests

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use utility::*;
 use std::net::SocketAddr;
 
 use http_body_util::combinators::BoxBody;
+use http_body_util::{BodyExt, Full};
 use hyper::body::Bytes;
 use hyper::server::conn::http1;
 use hyper::service::service_fn;
@@ -15,11 +16,26 @@ use tokio::net::TcpListener;
 async fn serve_cache(
     req: Request<hyper::body::Incoming>,
 ) -> Result<Response<BoxBody<Bytes, hyper::Error>>, hyper::Error> {
-    /* TODO: Connect to the remote HTTP(s) address if one is specified */
-    println!("({}){}", req.uri().scheme_str().unwrap_or_default(), req.uri().to_string());
-
+    let body_str;
     let mut tbd = Response::new(empty());
+
     *tbd.status_mut() = StatusCode::INTERNAL_SERVER_ERROR;
+
+    if req
+        .uri()
+        .scheme_str()
+        .is_some_and(|x| x.eq_ignore_ascii_case("http") || x.eq_ignore_ascii_case("https"))
+    {
+        body_str = "This request was correctly formatted but hasn't been implemented yet"
+    } else {
+        body_str = "This request wasn't formatted correctly"
+    }
+
+    /* TODO: Connect to the remote HTTP(s) address and cache data if one is specified */
+    *tbd.body_mut() = Full::new(body_str.into())
+        .map_err(|never| match never {})
+        .boxed();
+
     Ok(tbd)
 }
 
@@ -31,9 +47,11 @@ async fn serve(
         "GET" => serve_cache(req).await,
         "OPTIONS" => {
             let mut options = Response::new(empty());
-            options.headers_mut().append("Allow", "CACHE, GET, HEAD".parse().unwrap());
+            options
+                .headers_mut()
+                .append("Allow", "CACHE, GET, HEAD".parse().unwrap());
             Ok(options)
-        },
+        }
 
         _ => {
             let mut not_found = Response::new(empty());
