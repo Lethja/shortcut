085da162def46707867bcef12a91ed787d7ce610
Jason Lethbridge <lethbridgejason@gmail.com>
2024-03-09 17:32:15 +1100

    Made a pass-through proxy

--- src/main.rs
+++ src/main.rs
@@ -3,86 +3,123 @@ use utility::*;
 
 use std::net::SocketAddr;
 
-use http_body_util::combinators::BoxBody;
-use http_body_util::{BodyExt, Full};
-use hyper::body::Bytes;
-use hyper::server::conn::http1;
-use hyper::service::service_fn;
-use hyper::StatusCode;
-use hyper::{Request, Response};
+use bytes::Bytes;
+use http::{Method, StatusCode};
+use http_body_util::{combinators::BoxBody, BodyExt};
+use hyper::{server::conn::http1, service::service_fn, Request, Response};
 use hyper_util::rt::TokioIo;
 use tokio::net::TcpListener;
 
-async fn serve_cache(
-    req: Request<hyper::body::Incoming>,
-) -> Result<Response<BoxBody<Bytes, hyper::Error>>, hyper::Error> {
-    let body_str;
-    let mut tbd = Response::new(empty());
-
-    *tbd.status_mut() = StatusCode::INTERNAL_SERVER_ERROR;
-
-    if req
-        .uri()
-        .scheme_str()
-        .is_some_and(|x| x.eq_ignore_ascii_case("http") || x.eq_ignore_ascii_case("https"))
-    {
-        body_str = "This request was correctly formatted but hasn't been implemented yet"
-    } else {
-        body_str = "This request wasn't formatted correctly"
-    }
-
-    /* TODO: Connect to the remote HTTP(s) address and cache data if one is specified */
-    *tbd.body_mut() = Full::new(body_str.into())
-        .map_err(|never| match never {})
-        .boxed();
-
-    Ok(tbd)
-}
-
-async fn serve(
-    req: Request<hyper::body::Incoming>,
-) -> Result<Response<BoxBody<Bytes, hyper::Error>>, hyper::Error> {
-    match req.method().as_str().to_uppercase().as_str() {
-        "CACHE" => serve_cache(req).await,
-        "GET" => serve_cache(req).await,
-        "OPTIONS" => {
-            let mut options = Response::new(empty());
-            options
-                .headers_mut()
-                .append("Allow", "CACHE, GET, HEAD".parse().unwrap());
-            Ok(options)
-        }
-
-        _ => {
-            let mut not_found = Response::new(empty());
-            *not_found.status_mut() = StatusCode::METHOD_NOT_ALLOWED;
-            Ok(not_found)
-        }
-    }
-}
-
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8100));
+
     let listener = TcpListener::bind(addr).await?;
+    println!("Listening on http://{}", addr);
 
     loop {
         let (stream, _) = listener.accept().await?;
-
-        // Use an adapter to access something implementing `tokio::io` traits as if they implement
-        // `hyper::rt` IO traits.
         let io = TokioIo::new(stream);
 
-        // Spawn a tokio task to serve multiple connections concurrently
         tokio::task::spawn(async move {
-            // Finally, we bind the incoming connection to our `hello` service
             if let Err(err) = http1::Builder::new()
-                // `service_fn` converts our function in a `Service`
-                .serve_connection(io, service_fn(serve))
+                .preserve_header_case(true)
+                .title_case_headers(true)
+                .serve_connection(io, service_fn(handle_request))
+                .with_upgrades()
                 .await
             {
-                println!("Error serving connection: {:?}", err);
+                println!("Failed to serve connection: {:?}", err);
             }
         });
+    }
+}
+
+async fn handle_request(
+    req: Request<hyper::body::Incoming>,
+) -> Result<Response<BoxBody<Bytes, hyper::Error>>, hyper::Error> {
+    println!("req: {:?}", req);
+
+    match req.method().to_string().to_uppercase().as_str() {
+        "CONNECT" => {
+            if let Some(addr) = host_addr(req.uri()) {
+                tokio::task::spawn(async move {
+                    match hyper::upgrade::on(req).await {
+                        Ok(upgraded) => {
+                            if let Err(e) = tunnel(upgraded, addr).await {
+                                eprintln!("server io error: {}", e);
+                            };
+                        }
+                        Err(e) => eprintln!("upgrade error: {}", e),
+                    }
+                });
+
+                Ok(Response::new(empty()))
+            } else {
+                eprintln!("CONNECT host is not socket addr: {:?}", req.uri());
+                let mut resp = Response::new(full("CONNECT must be to a socket address"));
+                *resp.status_mut() = StatusCode::BAD_REQUEST;
+
+                Ok(resp)
+            }
+        }
+        "CACHE" => {
+            let host = match req.uri().host() {
+                Some(x) => x,
+                None => {
+                    let mut resp = Response::new(empty());
+                    *resp.status_mut() = StatusCode::BAD_REQUEST;
+
+                    return Ok(resp);
+                }
+            };
+
+            let port = req.uri().port_u16().unwrap_or(80);
+
+            let sender = build_sender(host, port).await;
+
+            let mut req = req;
+            *req.method_mut() = Method::GET;
+
+            let resp = sender?.send_request(req).await?;
+            Ok(resp.map(|b| b.boxed()))
+        }
+        "GET" => {
+            let host = match req.uri().host() {
+                Some(x) => x,
+                None => {
+                    let mut resp = Response::new(full(
+                        "Use this address in the 'http_proxy' environment variable instead\n",
+                    ));
+                    *resp.status_mut() = StatusCode::OK;
+                    return Ok(resp);
+                }
+            };
+
+            let port = req.uri().port_u16().unwrap_or(80);
+
+            let sender = build_sender(host, port).await;
+            let resp = sender?.send_request(req).await?;
+            Ok(resp.map(|b| b.boxed()))
+        }
+        "OPTIONS" => {
+            let mut options = Response::new(empty());
+
+            options
+                .headers_mut()
+                .append("Allow", "CACHE, GET, HEAD".parse().unwrap());
+
+            Ok(options)
+        }
+        _ => {
+            let mut unsupported = Response::new(empty());
+            *unsupported.status_mut() = StatusCode::METHOD_NOT_ALLOWED;
+
+            unsupported
+                .headers_mut()
+                .append("Allow", "CACHE, GET, HEAD".parse().unwrap());
+
+            Ok(unsupported)
+        }
     }
 }
--- src/utility.rs
+++ src/utility.rs
@@ -1,6 +1,11 @@
-use http_body_util::combinators::BoxBody;
-use http_body_util::{BodyExt, Empty, Full};
-use hyper::body::Bytes;
+use http_body_util::{combinators::BoxBody, BodyExt, Empty, Full};
+use hyper::{
+    body::{Bytes, Incoming},
+    client::conn::http1::{Builder, SendRequest},
+    upgrade::Upgraded,
+};
+use hyper_util::rt::TokioIo;
+use tokio::net::TcpStream;
 
 pub fn empty() -> BoxBody<Bytes, hyper::Error> {
     Empty::<Bytes>::new()
@@ -8,10 +13,48 @@ pub fn empty() -> BoxBody<Bytes, hyper::Error> {
         .boxed()
 }
 
-/* Remove if not needed by the time everything is up and working */
-#[allow(dead_code)]
 pub fn full<T: Into<Bytes>>(chunk: T) -> BoxBody<Bytes, hyper::Error> {
     Full::new(chunk.into())
         .map_err(|never| match never {})
         .boxed()
+}
+
+pub fn host_addr(uri: &http::Uri) -> Option<String> {
+    uri.authority().and_then(|auth| Some(auth.to_string()))
+}
+
+//Build a send request
+pub async fn build_sender(host: &str, port: u16) -> Result<SendRequest<Incoming>, hyper::Error> {
+    let stream = TcpStream::connect((host, port)).await.unwrap();
+    let io = TokioIo::new(stream);
+
+    let (sender, conn) = Builder::new()
+        .preserve_header_case(true)
+        .title_case_headers(true)
+        .handshake(io)
+        .await?;
+
+    tokio::task::spawn(async move {
+        if let Err(err) = conn.await {
+            println!("Connection failed: {:?}", err);
+        }
+    });
+
+    Ok(sender)
+}
+
+// Handle HTTPS requests (we can't cache these)
+pub async fn tunnel(upgraded: Upgraded, addr: String) -> std::io::Result<()> {
+    let mut server = TcpStream::connect(addr).await?;
+    let mut upgraded = TokioIo::new(upgraded);
+
+    let (from_client, from_server) =
+        tokio::io::copy_bidirectional(&mut upgraded, &mut server).await?;
+
+    println!(
+        "client wrote {} bytes and received {} bytes",
+        from_client, from_server
+    );
+
+    Ok(())
 }
